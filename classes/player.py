a365d8c464f68aebeef8fdd73f472e1f9baa23d7
Pierre Brivio <pierrebrivio14@gmail.com>
2021-12-13 15:56:52 +0100

    oui

--- classes/player.py
+++ classes/player.py
@@ -2,8 +2,8 @@
 
 class Player(object):
 
-  def __init__(self):
-    self._name = 'Player'
+  def __init__(self, name):
+    self._name = name
     self._score = 0
 
   def _get_name(self):
@@ -23,10 +23,9 @@ class Player(object):
 def initialize_players():
   players_list = []
   for i in range(2): # replace 2 by NB_OF_PLAYERS
-    player = Player()
     print('Vous êtes le joueur numéro ', i+1,', veuillez entrer votre nom : ')
     player_name = input()
-    player._set_name(player_name)
+    player = Player(player_name)
     players_list.append(player)
   for player in players_list:
     print('name : ', player._name, ', score : ', player._score)
