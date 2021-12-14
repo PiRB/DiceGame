898b7006143a95ec871e8ec80ce08f3dddbf3f55
simtaxx <souici.haris@gmail.com>
2021-12-14 15:46:42 +0100

    Oui

--- classes/game.py
+++ classes/game.py
@@ -1,14 +1,15 @@
 from classes.player import Player
+from constant import *
 
 class Game:
 
   def __init__(self):
     self._isFinished = False
 
-  def initialize_players(self):
+  def _initialize_players(self):
     players_list = []
-    for i in range(2): # replace 2 by NB_OF_PLAYERS
-      print('Vous êtes le joueur numéro ', i+1,', veuillez entrer votre nom : ')
+    for i in range(NB_OF_PLAYERS): # replace 2 by NB_OF_PLAYERS
+      print('Vous êtes le joueur numéro ', i + 1,', veuillez entrer votre nom : ')
       player_name = input()
       player = Player(player_name)
       players_list.append(player)
@@ -18,14 +19,26 @@ class Game:
       print('name : ', player._name, ', score : ', player._score) """
     return players_list
 
-  def start_game(self):
-    players = self.initialize_players()
-    for player in players:
-      print("%s c'est à toi: " % (player._get_name()))
-      print('------------------------------------')
+  def _start_game(self):
+    players = self._initialize_players()
+    max_player_score = 0
+    lap_index = 1
+    while MAX_SCORE_LIMIT > max_player_score:
+      for player in players:
+        print("%s c'est à toi: " % (player._get_name()))
+        print('------------------------------------')
+        print()
+        player._play_turn()
+        if max_player_score < player._get_score():
+          max_player_score = player._get_score()
+        print('Joueur %s || score: %s' % (player._get_name(), player._get_score()))
+        print('------------------------------------')
+        print()
+      print('Tour %s terminé, voici le classement' % (lap_index))
+      players_sorted = players
+      players_sorted.sort(key=lambda x: x._get_score(), reverse=True)
+      for index, player in enumerate(players_sorted):
+        print('%s --> %s || score: %s' % (index + 1, player._get_name(), player._get_score()))   
       print()
-      player._play_turn()
-      print('Joueur %s || score: %s' % (player._get_name(), player._get_score()))
       print('------------------------------------')
-      print()
-
+      lap_index += 1
