539c0dff0f27c3eee58dd7dadd45f3f4a30db9fc
LePetitLouis <44061639+LePetitLouis@users.noreply.github.com>
2021-12-15 16:31:55 +0100

    implement stats

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ ACE_BONUS_MULTIPLIER = 1000  # Special multiplier for aces bonus
 NB_DICE_TO_ROLL = 5
 
 NB_OF_PLAYERS = 2
-SCORE_TO_WIN = 500
+SCORE_TO_WIN = 1000
 
+# Stats
+MAX_TURN_SCORING = { 'player': '', 'score': 0}
+LONGUEST_TURN = { 'player': '', 'roll': 0}
+MAX_TURN_LOSS = { 'player': '', 'score': 0}
+MEAN_SCORING_TURN = []
+MEAN_NO_SCORING_TURN = []
 
 # return a list of dices value occurrence for a roll of nb_dice_to_roll dices
 def roll_dice_set(nb_dice_to_roll):
@@ -57,11 +63,8 @@ def analyse_score(dice_value_occurrence_list):
 def initialize_players():
   players_list = []
   for i in range(NB_OF_PLAYERS):
-    print('Entrez votre nom joueur ', i+1)
-    player_name = input()
-    start_score = 0
-    player = {'name': player_name, 'score': start_score, 'turn': 0, 'nb_roll': 0, 'nb_bonus': 0}
-    players_list.append(player)
+    player_name = input(f"Entrez votre nom joueur {i+1}: ")
+    players_list.append({'name': player_name, 'score': 0, 'turn': 1, 'nb_roll': 0, 'nb_bonus': 0})
 
   return players_list
 
@@ -72,12 +75,10 @@ def sort_players(players_list):
   players_list.sort(key=get_score, reverse=True)
   return players_list
 
-def hasWon(players_list):
-
-  players_list = sort_players(players_list)
-  
-  if players_list[0]['score'] >= SCORE_TO_WIN:
-     return True
+def hasWon(player):
+  if player['score'] >= SCORE_TO_WIN:
+    print(f"La partie est finie, {player['name']} a atteint ou dépassé les {SCORE_TO_WIN} points")
+    return True
   else:
     return False
 
@@ -96,31 +97,38 @@ def set_highest_number_of_turn(player, highest_nb_turn):
   return highest_nb_turn
 
 def play_turn(player, highest_nb_turn):
-  nb_roll = 1
-  isRerolling = True
+  nb_roll, isRerolling = 1, True
   player_roll = analyse_score(roll_dice_set(NB_DICE_TO_ROLL))
   player['score'] += player_roll[0]
   get_number_of_bonus(player_roll, player)
   player['turn'] += 1
   highest_nb_turn = set_highest_number_of_turn(player, highest_nb_turn)
-  print("Roll #",nb_roll ,"Vous avez scoré : ",player_roll[0], ", vous avez maintenant : ", player['score'])
+  print(f"roll #{nb_roll} : scoring {player_roll[0]} vous avez maintenant {player['score']}")
 
   nb_dice_to_reroll = sum(player_roll[1])
+  MEAN_SCORING_TURN.append(player_roll[0])
+
+  if player_roll[0] > MAX_TURN_SCORING['score']: 
+    MAX_TURN_SCORING['player'] = player['name']
+    MAX_TURN_SCORING['score'] = player_roll[0]
   if nb_dice_to_reroll == 0:
     isRerolling = False
     print("Vous n'avez plus de dés pour jouer, c'est au joueur suivant")
   
   while isRerolling and nb_dice_to_reroll != 0:
     new_nb_dice_to_reroll = 0
-    print('Voulez-vous relancer les ', nb_dice_to_reroll, ' dé(s) restant(s) ( y/n )')
+    print(f"Voulez-vous relancer les {nb_dice_to_reroll} dé(s) restant(s) ( y/n )")
     reroll_response = input()
     if reroll_response == 'y':
-
       nb_roll +=1
+      if nb_roll > LONGUEST_TURN['roll']:
+        LONGUEST_TURN['player'] = player['name']
+        LONGUEST_TURN['roll'] = nb_roll
+
       player_roll = analyse_score(roll_dice_set(nb_dice_to_reroll))
       player['score'] += player_roll[0]
       print()
-      print("Roll #",nb_roll ,"Vous avez scoré : ",player_roll[0]," avec ", get_scoring_dice(nb_dice_to_reroll), " dé(s), vous avez maintenant : ", player['score'])
+      print(f"roll #{nb_roll} : scoring {player_roll[0]} avec {get_scoring_dice(nb_dice_to_reroll)} dé(s), vous avez maintenant : {player['score']}")
       new_nb_dice_to_reroll = sum(player_roll[1])
       player['nb_roll'] += nb_roll
 
@@ -132,6 +140,10 @@ def play_turn(player, highest_nb_turn):
     if nb_dice_to_reroll == new_nb_dice_to_reroll:
       print("Vous n'avez pas marqué sur votre nouveau lancé, CHEH !")
       print()
+      MEAN_NO_SCORING_TURN.append(player_roll[0])
+      if player_roll[0] > MAX_TURN_LOSS['score']: 
+        MAX_TURN_LOSS['player'] = player['name']
+        MAX_TURN_LOSS['score'] = player_roll[0]
       player['score'] = 0
       break
 
@@ -142,16 +154,11 @@ def play_turn(player, highest_nb_turn):
 def display_turn_finished(players_list):
   players_list = sort_players(players_list)
 
-def set_total_nb_of_turn(player, total_nb_turn):
-  total_nb_turn.append(player['turn'])
-  return total_nb_turn
-
 def main():
   turn_count = 1
   isFinished = False
   players_list = initialize_players()
   highest_nb_turn = 0
-  total_nb_turn = []
 
   while isFinished == False:
     for index, player in enumerate(players_list):
@@ -170,19 +177,24 @@ def main():
     sorted_players = sort_players(players_list)
     print("Classement: ")
     for index, player in enumerate(sorted_players):
-      print("-", index+1, player['name'] ," a un score de: ", player['score'])
+      print(f"-{index+1} {player['name']} a un score de: {player['score']}")
 
   print()
-  print("Voici le résultat de la partie")
-  print('Partie en', return_of_play_turn[1], 'tours')
+  print(f"Game in {return_of_play_turn[1]} turns")
   players_list = sort_players(players_list)
   for player in players_list:
-    total_nb_turn = set_total_nb_of_turn(player, total_nb_turn)
     if player['score'] >= SCORE_TO_WIN:
-      print(player['name'], " WIN ! scoring ", player['score'], " points avec", player['nb_bonus'] ,"bonus, en ", player['turn']," tours", "roll", player['nb_roll'])
+      print(f"{player['name']} WIN ! scoring {player['score']} points avec {player['nb_bonus']} bonus, en {player['turn']} tours roll {player['nb_roll']}")
     else:
-      print(player['name'], " LOSE ! scoring ", player['score'], " points avec", player['nb_bonus'] ,"bonus, en ", player['turn']," tours", "roll", player['nb_roll'])
-  print(sum(total_nb_turn), "tours on été joué en tout")
+      print(f"{player['name']} LOSE ! scoring {player['score']} points avec {player['nb_bonus']} bonus, en {player['turn']} tours roll {player['nb_roll']}")
+  
+  print("\n---- STATS ----")
+  print(f"Max turn scoring : {MAX_TURN_SCORING['player']} with {MAX_TURN_SCORING['score']}")
+  print(f"Longest turn : {LONGUEST_TURN['player']} with {LONGUEST_TURN['roll']}")
+  print(f"Max turn loss : {MAX_TURN_LOSS['player']} with {MAX_TURN_LOSS['score']}")
+
+  print(f"\nMean scoring turn : {sum(MEAN_SCORING_TURN) / len(MEAN_SCORING_TURN)}")
+  print(f"Mean non scoring turn : {sum(MEAN_NO_SCORING_TURN) / len(MEAN_NO_SCORING_TURN)}")
   return 0
 
 main()
